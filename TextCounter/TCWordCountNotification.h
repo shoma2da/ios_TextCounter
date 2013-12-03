bf9dc8bbe812d0dbd84f7f35c19e943cf9441392
Shoichi Matsuda <marcos.shoichi.matsuda@gmail.com>
2013-12-03 23:35:09 +0900

    通知用のクラスを作成

---
+++ TextCounter/TCWordCountNotification.h
@@ -0,0 +1,13 @@
+//
+//  TCWordCountNotification.h
+//  TextCounter
+//
+//  Created by Shoichi Matsuda on 2013/12/03.
+//  Copyright (c) 2013年 Shoichi Matsuda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TCWordCountNotification : NSObject
+
+@end
---
+++ TextCounter/TCWordCountNotification.m
@@ -0,0 +1,13 @@
+//
+//  TCWordCountNotification.m
+//  TextCounter
+//
+//  Created by Shoichi Matsuda on 2013/12/03.
+//  Copyright (c) 2013年 Shoichi Matsuda. All rights reserved.
+//
+
+#import "TCWordCountNotification.h"
+
+@implementation TCWordCountNotification
+
+@end
